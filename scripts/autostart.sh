73cedc51c33fab17e0b19b181ac76106aa90a283
Raga Subekti <ragasubekti@gmail.com>
2023-09-18 10:44:38 +0900

    autochange refresh rate to 360Hz on autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-/home/titus/github/dwm-titus/scripts/status &
+DISPLAY_OUT="DP-1"
+DISPLAY_RATE="360.00"
+
+$HOME/Projects/dwm/scripts/status &
 /usr/bin/lxpolkit &
-/usr/bin/synergy &
-feh --bg-fill --randomize --recursive $HOME/Pictures/backgrounds/ &
+feh --bg-fill $HOME/Pictures/background.jpg &
 picom --config "$HOME/.config/picom.conf" &
-xset s off -dpms #disabling things like turning off the monitor
+xset s off -dpms
 xsetroot -cursor_name left_ptr
+xrandr --output "${DISPLAY_OUT}" --mode 1920x1080 --rate "${DISPLAY_RATE}"
